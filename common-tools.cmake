72feb73bfe0d57e937637f811326cde80f3da767
Sergey Ladanov <sergei_ladanov_93@mail.ru>
2024-08-12 23:54:47 +0300

    reduce cmake tools

--- common-tools.cmake
+++ common-tools.cmake
@@ -9,8 +9,6 @@ endif()
 # Установка папки с компонентами проекта
 set(COMPONENTS_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/Components)
 
-# Путь к проекту
-set(Project_Path ${CMAKE_CURRENT_SOURCE_DIR})
 
 # Получение списка каталогов для заголовочных файлов исходного проекта
 get_target_property(MainIncludes ${TARGET_NAME} INCLUDE_DIRECTORIES)
@@ -20,8 +18,6 @@ if(NOT MainIncludes)
     set(MainIncludes "")
 endif()
 
-# Вспомогательная переменная для сканирования вложенных зависимотсей компонентов
-set(__dependency_tree "")
 
 # Получение списка компонентов в заданном каталоге
 macro(__Get_DirectorytList result curdir)
@@ -39,117 +35,11 @@ macro(__Get_DirectorytList result curdir)
     set(${result} ${dirlist})
 endmacro()
 
-# Получение списка каталогов с заголовочными файлами в заданной директории
-macro(__Get_IncludeDirectories result curdir)
-    set(dirlist "")
-    message(STATUS "Adding include directories from dependency \"${curdir}\" to \"${PROJECT_NAME}\"")
-    file(READ ${curdir}/CMakeLists.txt text)
-    # Find include directories call
-    string(REGEX MATCHALL "[I,i][n,N][c,C][l,L][u,U][d,D][e,E]_[d,D][i,I][r,R][e,E][c,C][t,T][o,O][r,R][i,I][e,E][s,S]\\([^\\(]*\\)" out_var ${text})
-    # Remove key word "include_directories"
-    string(REGEX REPLACE "[I,i][n,N][c,C][l,L][u,U][d,D][e,E]_[d,D][i,I][r,R][e,E][c,C][t,T][o,O][r,R][i,I][e,E][s,S]" "" out_var "${out_var}")
-    # Remove brackets and spaces"
-    string(REGEX REPLACE "\\([\r\n\t ]*|[\r\n\t ]*\\)" "" out_var "${out_var}")
-    # Remove quotes
-    string(REGEX REPLACE "\"" "" out_var "${out_var}")
-    # Convert to list
-    string(REGEX REPLACE "[\r,\n,\t, ][\r\n\t ]*" ";" out_var "${out_var}")
-    
-    foreach(item ${out_var})
-        # set(item ${curdir}/${item})
-        list(APPEND dirlist ${curdir}/${item})
-    endforeach()
-
-    set(${result} ${dirlist})
-endmacro()
-
-
-
-
-
-# Получение списка каталогов с заголовочными файлами в заданной директории
-macro(__Get_Dependencies result curdir)
-    set(dirlist "")
-    message(STATUS "Adding dependency from \"${curdir}\" to \"${PROJECT_NAME}\"")
-    file(READ ${curdir}/CMakeLists.txt text)
-    # Find include directories call
-    string(REGEX MATCHALL "[D,d][e,E][p,P][e,N][n,N][d,D][s,S]_[o,O][n,N]\\([^\\(]*\\)" out_var ${text})
-    # Remove key word "include_directories"
-    string(REGEX REPLACE "[D,d][e,E][p,P][e,N][n,N][d,D][s,S]_[o,O][n,N]" "" out_var "${out_var}")
-    # Remove brackets and spaces"
-    string(REGEX REPLACE "\\([\r\n\t ]*|[\r\n\t ]*\\)" "" out_var "${out_var}")
-    # Remove quotes
-    string(REGEX REPLACE "\"" "" out_var "${out_var}")
-    # Convert to list
-    string(REGEX REPLACE "[\r,\n,\t, ][\r\n\t ]*" ";" out_var "${out_var}")
-    
-    foreach(item ${out_var})
-        # set(item ${curdir}/${item})
-        list(APPEND dirlist ${item})
-    endforeach()
-
-    set(${result} ${dirlist})
-endmacro()
-
-
-# Получение списка файлов с исходным кодом
-macro(__SourceFilesPresent result curdir)
-    file(READ ${curdir}/CMakeLists.txt text)
-    string(REGEX MATCHALL "[a,A][d,D][d,D]_[l,L][i,I][b,B][r,R][a,A][r,R][y,Y]" out_var ${text})
-    if (out_var)
-        set(${result} "True")
-    else()
-        set(${result} "")
-    endif()
-    
-endmacro()
 
 # Получение списка компонентов проекта
 __Get_DirectorytList(COMPONENT_LIST ${COMPONENTS_DIRECTORY})
 
 message(STATUS "Final components list: \"${COMPONENT_LIST}\"")
-
-
-#-----------------------------------------------
-# Обработка дерева зависимостей
-macro(__HandleDependencyTree name)
-
-    list(FIND __dependency_tree ${name} res)
-
-    if (res GREATER -1)
-        message(STATUS "Dependency \"${name}\" already included") 
-    else()
-        list(APPEND __dependency_tree ${name})
-        __Get_Dependencies(res ${COMPONENTS_DIRECTORY}/${name})
-
-        foreach(item ${res})
-            __HandleDependencyTree(${item})
-        endforeach()
-    
-    endif()
-    
-endmacro()
-
-
-# Указать имя зависимость для компонента
-function(__Depends_On name)
-    message(STATUS "Handle dependency \"${name}\" for \"${PROJECT_NAME}\"")
-    set(dirs "")
-
-    # Put directory name to COMPONENT_NAME variable
-    get_filename_component(COMPONENT_NAME ${CMAKE_CURRENT_SOURCE_DIR} NAME)
-
-    if (NOT ${COMPONENT_NAME} STREQUAL ${name})
-        __Get_IncludeDirectories(dirs ${COMPONENTS_DIRECTORY}/${name})
-        __SourceFilesPresent(src_files ${COMPONENTS_DIRECTORY}/${name})
-        target_include_directories(${PROJECT_NAME} PRIVATE ${dirs})
-
-        if (src_files)
-            target_link_libraries(${PROJECT_NAME} ${name})
-        endif()
-    endif()
-
-endfunction()
 
 
 # Указать список заивисимостей от других компнентов
@@ -161,16 +51,11 @@ macro( Depends_On )
     cmake_parse_arguments(_ARGUMENTS "${_OPTIONS_ARGS}" "${_ONE_VALUE_ARGS}"  ${ARGN} )
 
     foreach(item ${ARGN})
-        __HandleDependencyTree(${item})
+        #__HandleDependencyTree(${item})
+        message(STATUS "Depends from ${item} for ${PROJECT_NAME}")
+        target_link_libraries(${PROJECT_NAME} PUBLIC ${item})
+        # add_dependencies(${PROJECT_NAME} ${item})
     endforeach()
-
-    message(STATUS "Final dependency tree for ${CMAKE_CURRENT_SOURCE_DIR}: ${__dependency_tree}")
-
-
-    foreach(item ${__dependency_tree})
-        __Depends_On(${item})
-    endforeach()
-
 endmacro()
 
 # Указать зависимость от главного приложения
@@ -182,9 +67,11 @@ endfunction()
 
 # Указать, что данный компонент зависит от всех остальных
 function(Depends_All)
-    if (COMPONENT_LIST)
+    if (COMPONENT_LIST) 
         foreach(item ${COMPONENT_LIST})
-            __Depends_On(${item})
+            if (NOT ${PROJECT_NAME} STREQUAL ${item})
+                Depends_On(${item})
+            endif()
         endforeach()
     endif()
 endfunction()
@@ -192,15 +79,7 @@ endfunction()
 # Зарегистрировать компонент
 function(__Register_Component name)
     add_subdirectory(Components/${name})
-    get_property(inc_dirs DIRECTORY ${COMPONENTS_DIRECTORY}/${name} PROPERTY INCLUDE_DIRECTORIES)
-    __SourceFilesPresent(src_files ${COMPONENTS_DIRECTORY}/${name})
-
-    target_include_directories(${TARGET_NAME} PRIVATE ${inc_dirs})
-
-    if (src_files)
-        target_link_libraries(${TARGET_NAME} PRIVATE ${name})
-    endif()
-    
+    target_link_libraries(${TARGET_NAME} PRIVATE ${name})
 endfunction()
 
 # Добавить компоненты в проект
