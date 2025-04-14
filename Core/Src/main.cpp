3d8196f3e906704995bb89355ea1e328fce8849f
Sergey Ladanov <sergei_ladanov_93@mail.ru>
2025-04-14 22:27:45 +0300

    add flags

--- Core/Src/main.cpp
+++ Core/Src/main.cpp
@@ -1,14 +1,18 @@
 
 #include "main.hpp"
 #include "IncludeOnlyComponent.hpp"
+#include <cstdint>
+#include <cstdlib>
 
 void ExtTest(void)
 {
+
 }
 
 // Основная программа
 int main(void)
 {
+	ExtTest();
 	include_only_print();
 	Hello_From_C();
 	Hello_From_CPP();
