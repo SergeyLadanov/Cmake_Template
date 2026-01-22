02125a0a325fdcfbc9ca1a02c5dd612ea3f6c5c8
Sergey Ladanov <sergei_ladanov_93@mail.ru>
2026-01-22 10:16:14 +0300

    Временное решение для работы intelsense vs code

--- Core/Src/main.cpp
+++ Core/Src/main.cpp
@@ -4,17 +4,25 @@
 #include <cstdint>
 #include <cstdlib>
 
-import math;
+#if INTELSENSE == 1
+  #include "math.cppm"
+#else
+  import math;
+#endif
+
 
 void ExtTest(void)
 {
 
 }
 
+Test myClass;
+
 // Основная программа
 int main(void)
 {
 	printf("Test module result %d\r\n", add(1, 2));
+	myClass.Action();
 	ExtTest();
 	include_only_print();
 	Hello_From_C();
--- Core/Src/math.cppm
+++ Core/Src/math.cppm
@@ -1,9 +1,31 @@
 
 module;
-
+#include <cstdio>
 export module math;
 
 export int add(int a, int b)
 {
     return a + b;
 }
+
+export class Test
+{
+private:
+    /* data */
+public:
+    Test(/* args */)
+    {
+
+    }
+
+    void Action(void)
+    {
+        printf("Test::Action\n");
+    }
+
+    ~Test()
+    {
+
+    }
+};
+
